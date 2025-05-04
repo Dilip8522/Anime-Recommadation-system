681e74b9d60acaf7b6ceb4c5233512c7d47c2fb4
Akula Dilipkumar <125907258+Dilip8522@users.noreply.github.com>
2025-05-05 00:35:46 +0530

    Delete db directory

--- db/config.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import connect
-def init_db():
-    connect(
-        db="Dsp",
-        username="devisriprasad948",
-        password="123",
-        host="mongodb+srv://devisriprasad948:123@cluster0.tz2oa.mongodb.net/"
-    )
