f6f2e31992e597e29f9e2dea9459ee239765130c
Akula Dilipkumar <125907258+Dilip8522@users.noreply.github.com>
2025-05-05 00:38:12 +0530

    Delete user.py

--- user.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import StringField, Document, IntField, EmailField
-
-class User(Document):
-    name=StringField(required=True)
-    email=EmailField(required=True)
-    password=StringField(required=True)
-    age=IntField(required=True)
