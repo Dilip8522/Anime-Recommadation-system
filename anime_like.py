c09e4bceb54234330f761e6bdc160d9ab9aa0b24
Akula Dilipkumar <125907258+Dilip8522@users.noreply.github.com>
2025-05-18 10:58:29 +0530

    Delete anime_like.py

--- anime_like.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import EmailField, Document, IntField, BooleanField
-
-class AnimeLike(Document):
-    anime_id = IntField(required=True)
-    email = EmailField(required=True)
-    is_liked = BooleanField(default=True) 
